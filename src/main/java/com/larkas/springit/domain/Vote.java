a013d7b35b99bd0779bed283a17f6aa5e266a57d
laerkas <penta1981>
2021-08-24 18:21:29 +0200

    Fixed more warnings due to lombok problems

--- src/main/java/com/larkas/springit/domain/Vote.java
+++ src/main/java/com/larkas/springit/domain/Vote.java
@@ -1,19 +1,36 @@
 package com.larkas.springit.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
-@NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Vote {
 
     @Id
     @GeneratedValue
     private Long id;
     private int vote;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Vote vote = (Vote) o;
+
+        return Objects.equals(id, vote.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1634544760;
+    }
 }
